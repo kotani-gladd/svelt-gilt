77bef0f1ded770d50df25f6026c08b569942d6ca
kotani-gladd <kotani@labellevie.jp>
2025-12-16 18:37:34 +0900

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+    import Navbar from "../components/Navbar.svelte";
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<Navbar/>
+{@render children()}
