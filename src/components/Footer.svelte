b259ed3f9f143e989e503ebe0edb3fa5d74a9133
kotani-gladd <kotani@labellevie.jp>
2025-12-29 14:23:39 +0900

    footerの修正

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -33,36 +33,42 @@
 	<div class="container text-body-secondary">
 		<div class="row pb-3">
 			<div class="col-lg-7">
-				<img src="src/img/logo_gilt.png" height="16" width="80"/>
+				<div class="d-flex justify-content-center justify-content-md-start pb-2">
+					<img src="src/img/logo_gilt.png" height="16" width="80"/>
+				</div>
 			</div>
 			<div class="col-lg-5 text-lg-end">
 				<div class="d-flex justify-content-center justify-content-md-end gap-4">
-					<div class=""><img src="src/img/icon_instagram.png" height="19" width="19"/></div>
-					<div class=""><img src="src/img/icon_youtube.png" height="18" width="26"/></div>
-					<div class=""><img src="src/img/icon_lindkedin.png" height="17" width="17"/></div>
-					<div class=""><img src="src/img/icon_line.png" height="24" width="24"/></div>
+					<a class=""><img src="src/img/icon_instagram.png" height="19" width="19"/></a>
+					<a class=""><img src="src/img/icon_youtube.png" height="18" width="26"/></a>
+					<a class=""><img src="src/img/icon_lindkedin.png" height="17" width="17"/></a>
+					<a class=""><img src="src/img/icon_line.png" height="24" width="24"/></a>
 				</div>
 			</div>
 		</div>
-		<div class="d-flex flex-row flex-md-column flex-wrap justify-content-center justify-content-md-start">
-			<div class="c-footer-text">GILTについて</div>
-			<div class="c-footer-text">ショッピングガイド</div>
-			<div class="c-footer-text">お問い合わせ</div>
-			<div class="c-footer-text">会社概要</div>
-			<div class="c-footer-text">個人情報保護方針</div>
-			<div class="c-footer-text">特定商取引に関する表示</div>
-		</div>
-		<div class="row pb-3">
-			<div class="col-12">
-				<div class="d-flex justify-content-center justify-content-md-end gap-4">
-					<div class=""><img src="src/img/icon_app_store.png" height="35" width="94"/></div>
-					<div class=""><img src="src/img/icon_google_play.png" height="35" width="116"/></div>
+		<div class="d-flex flex-column-reverse flex-md-column">
+			<div class="d-flex flex-row flex-md-column flex-wrap justify-content-center justify-content-md-start">
+				<div class="c-footer-text pe-3 pb-2">GILTについて</div>
+				<div class="c-footer-text pe-3 pb-2">ショッピングガイド</div>
+				<div class="c-footer-text pe-3 pb-2">お問い合わせ</div>
+				<div class="c-footer-text pe-3 pb-2">会社概要</div>
+				<div class="c-footer-text pe-3 pb-2">個人情報保護方針</div>
+				<div class="c-footer-text pe-3 pb-2">特定商取引に関する表示</div>
+			</div>
+			<div class="row pb-3">
+				<div class="col-12">
+					<div class="d-flex justify-content-center justify-content-md-end gap-4">
+						<div class=""><img src="src/img/icon_app_store.png" height="35" width="94"/></div>
+						<div class=""><img src="src/img/icon_google_play.png" height="35" width="116"/></div>
+					</div>
 				</div>
 			</div>
 		</div>
 		<div class="row pb-3">
 			<div class="col-12">
-				<div class="c-footer-span">©la belle vie 株式会社 ALL RIGHTS RESERVED.</div>
+				<div class="d-flex justify-content-center justify-content-md-start">
+					<div class="c-footer-span">©la belle vie 株式会社 ALL RIGHTS RESERVED.</div>
+				</div>
 			</div>
 		</div>
 	</div>
